59334907d79d73b4816a2c86aa7bf20eb2d86206
wise-penny <inland14@live.com>
2024-08-23 19:31:33 -0400

    Rollback for the clear-node action

--- example/dotpipe.js
+++ example/dotpipe.js
@@ -783,7 +783,7 @@ function pipes(elem, stop = false) {
         var pages = elem.getAttribute("insert").split(";");
         pages.forEach((e) => {
             console.log(e);
-            document.getElementById(e).remove();
+            document.getElementById(e).innerHTML = "";
         });
     }
     if (elem.classList.contains("multi-part") == true) {
